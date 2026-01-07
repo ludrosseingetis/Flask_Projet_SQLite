99babd08d5b0b6bde9692b6c6f2e6eff03ed918e
ludrosse <144143227+ludrosseingetis@users.noreply.github.com>
2026-01-07 15:58:05 +0100

    bonnormalement

--- __init__.py
+++ __init__.py
@@ -46,32 +46,29 @@ def authentification():
     return render_template('formulaire_authentification.html', error=False)
     return render_template('formulaire_authentification.html', error=False)
     
-@app.route('/authentification', methods=['GET', 'POST'])
+@app.route('/authentificationnom', methods=['GET', 'POST'])
 def authentificationnom():
     if request.method == 'POST':
-        # Vérifier les identifiants
-        if request.form['username'] == 'admin' and request.form['password'] == 'password': # password à cacher par la suite
+        if request.form['username'] == 'admin' and request.form['password'] == 'password': 
             session['authentifie'] = True
-            # Rediriger vers la route lecture après une authentification réussie
             return redirect(url_for('lecture'))
         else:
-            # Afficher un message d'erreur si les identifiants sont incorrects
             return render_template('formulaire_authentification.html', error=True)
 
     return render_template('formulaire_authentification.html', error=False)
+
 @app.route('/fiche_nom/<string:post_nom>')
 def ReadficheNom(post_nom):
+    if not session.get('authentifie'):
+        return redirect(url_for('authentificationnom'))
+    
     conn = sqlite3.connect('database.db')
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM clients WHERE nom LIKE ?', (post_nom,))
     data = cursor.fetchall()
     conn.close()
-       if not est_authentifie():
-        # Rediriger vers la page d'authentification si l'utilisateur n'est pas authentifié
-        return redirect(url_for('authentification'))
-
-        return render_template('read_data.html', data=data)
-
+    
+    return render_template('read_data.html', data=data)
 @app.route('/fiche_client/<int:post_id>')
 def Readfiche(post_id):
     conn = sqlite3.connect('database.db')
