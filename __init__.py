44eb6b8e497a2a339a414ac3f2f67bf94aff68c5
ludrosse <144143227+ludrosseingetis@users.noreply.github.com>
2026-01-07 15:40:14 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -43,7 +43,7 @@ def authentification():
 def ReadficheNom(post_nom):
     conn = sqlite3.connect('database.db')
     cursor = conn.cursor()
-    cursor.execute('SELECT * FROM clients WHERE nom = ?', (post_nom,))
+    cursor.execute('SELECT * FROM clients WHERE nom LIKE ?', (post_nom,))
     data = cursor.fetchall()
     conn.close()
     # Rendre le template HTML et transmettre les données
