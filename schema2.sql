5b600b5c9f62e3db83fbf992219b9f2aa7689187
ludrosse <144143227+ludrosseingetis@users.noreply.github.com>
2026-02-18 14:41:59 +0100

    Update schema2.sql

--- schema2.sql
+++ schema2.sql
@@ -3,13 +3,15 @@ CREATE TABLE clients (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     nom TEXT NOT NULL,
-    prenom TEXT NOT NULL
+    prenom TEXT NOT NULL -- Pas de virgule ici !
 );
+
 DROP TABLE IF EXISTS taches;
 CREATE TABLE taches (
-       id_tache INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_tache INTEGER PRIMARY KEY AUTOINCREMENT,
     created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     titre TEXT NOT NULL,
     description TEXT NOT NULL,
-    id_client TEXT NOT NULL
-    );
+    id_client INTEGER NOT NULL, -- Mettre INTEGER pour correspondre à l'ID client
+    FOREIGN KEY(id_client) REFERENCES clients(id) -- Pour lier les tables
+);
