bad482dfa0f5877e613af1e5e9b4137fa68c6740
ludrosse <144143227+ludrosseingetis@users.noreply.github.com>
2026-02-18 16:06:11 +0100

    ,kl

--- schema2.sql
+++ schema2.sql
@@ -11,8 +11,6 @@ DROP TABLE IF EXISTS taches;
 
 CREATE TABLE taches (
     id_tache INTEGER PRIMARY KEY AUTOINCREMENT,
-    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, -- Date de création auto
-    date_echeance TEXT, -- Ta colonne de date (optionnelle)
     titre TEXT NOT NULL,
     description TEXT NOT NULL
 );
